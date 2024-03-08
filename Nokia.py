1a6d141c6d2c7b109f4e42347da028b15c02926a
NOKIA <134571607+NOKIA-404@users.noreply.github.com>
2024-03-09 01:04:54 +0500

    Update Nokia.py

--- Nokia.py
+++ Nokia.py
@@ -1,6 +1,6 @@
 import os,platform
 os.system('git pull')
 # exit(' Wait Tool On updating ')
-Brand=platform.architecture()[0]
-if Brand=="32bit":__import__("Nokia32")
-elif Brand=="64bit":__import__("Nokia64")
+Nokia=platform.architecture()[0]
+if Nokia=="32bit":__import__("Nokia32")
+elif Nokia=="64bit":__import__("Nokia64")
