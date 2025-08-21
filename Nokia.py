03b7576e560951e3bf62becfc7f4029894040f4e
NOKIA <134571607+NOKIA-404@users.noreply.github.com>
2025-08-21 11:40:53 +0500

    Update Nokia.py

--- Nokia.py
+++ Nokia.py
@@ -1,1 +1,23 @@
-import ssb_enc
+#####apni 
+ #coding=utf-8
+import os, sys, platform
+ 
+os.system('git pull')
+ 
+try:
+    if sys.argv[1]=='update':
+        os.system('git pull')
+except:
+    pass
+ 
+ 
+bit = platform.architecture()[0]
+if bit == '64bit':
+    
+        import ssb_enc
+    
+ 
+elif bit == '32bit':
+    
+        import ssb1_enc
+ 
